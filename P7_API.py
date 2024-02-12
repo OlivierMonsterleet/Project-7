1ed51af37ccf09a89c56e9835296bf330827f136
OlivierMonsterleet <146861945+OlivierMonsterleet@users.noreply.github.com>
2024-02-12 16:19:41 +0100

    Add files via upload

--- P7_API.py
+++ P7_API.py
@@ -6,26 +6,13 @@
 # In[ ]:
 
 
-# !pip install plost
-
-
-# In[20]:
-
-
-# pip install pytest
-
-
-# In[ ]:
-
-
 
 
 
 # In[1]:
 
 
-from flask import Flask
-from flask import request
+from flask import Flask # ou fast API
 import requests
 import pickle
 import json
@@ -34,33 +21,48 @@ import pandas as pd
 import numpy as np
 
 
-# In[3]:
+# In[ ]:
 
 
-app = Flask(__name__)
-
-
-data = pickle.load(open('data_opti_metier.pkl','rb'))
-model = pickle.load(open('model_opti_metier.pkl','rb'))
-
-app
-
-
-# In[18]:
-
-
-#url = 'http://127.0.0.1:8050/'
+#!pip install plost
 
 
 # In[ ]:
 
 
-#   http://127.0.0.1:8050/get_client_data/248265
 
 
 
+# In[4]:
 
-# In[7]:
+
+app = Flask(__name__)
+data = pickle.load(open('mydata.pkl','rb'))
+model = pickle.load(open('mymodel.pkl','rb'))
+app
+
+
+# In[2]:
+
+
+url = 'http://127.0.0.1:8050/'
+url
+
+
+# In[6]:
+
+
+#data = data.drop(columns=['index','TARGET'])
+data
+
+
+# In[ ]:
+
+
+data.dtypes
+
+
+# In[5]:
 
 
 @app.route("/hello/")
@@ -68,32 +70,40 @@ def hello():
         return json.dumps("hello")
 
 
-# # get_all_data en dictionnary
+# # get_all_data-->dictionnary
 
-# In[8]:
+# In[6]:
 
 
-@app.route("/get_all_data/",methods=['GET'])
-def get_all_data():
+@app.route("/get_all_data/")
+def get_all_data5():
     return (data.to_dict())
+    #return (data.csv())
+    #return (data.to_dict(orient='records'))
+    #return (data.to_json())
 
 
+# # get_all_data-->JSON
 
-# In[11]:
+# In[7]:
 
 
-@app.route("/get_all_data_json/",methods=['GET'])
+@app.route("/get_all_data_json/")
 def get_all_data_json():
     return (data.to_json())
 
 
-# In[16]:
+# # get_client_data
+
+# In[8]:
 
 
-@app.route("/get_client_data",methods=['GET'])
-def get_client_data():
-    cid=request.args.get('cid')
+@app.route("/get_client_data/<cid>")
+def get_client_data17(cid):
+    cid = int(cid) # transfo en integer à forcer
     data_filtered = data.loc[data['SK_ID_CURR']==cid]
+
+    #return (data_filtered.to_dict(orient='index'))
     return (data_filtered.to_json())
 
 
@@ -102,66 +112,150 @@ def get_client_data():
 
 
 
-# In[10]:
+
+# # get_prediction(client_id)
+
+# In[23]:
 
 
-@app.route("/get_client_prediction", methods=['GET'])
-def get_client_prediction():
-    cid=request.args.get('cid')
+@app.route("/get_client_prediction/<cid>")
+def get_client_prediction5(cid):
+    cid = int(cid) # transfo en integer à forcer
+    
     data_filtered = data.loc[data['SK_ID_CURR']==cid]
     data_filtered = data_filtered.drop(columns=['TARGET','SK_ID_CURR']) 
     pred = model.predict(data_filtered)
-    return str(pred[0])  # mettre zéro le model retourne un dataframe
+    #prediction = classifier.predict_proba(filtered_data).tolist()
+    if(pred>0.65): #[0][1]
+        avis="Le crédit est refusé"
+    else:
+        avis="Le crédit est accepté"
+    print(avis)
+    print(pred)
+    #return {'prediction': avis, proba_rembour':round(prediction[0][0],2)}
+    return {str(pred[0]), avis}  # mettre zéro le model retourne un dataframe
     
 
 
-# In[13]:
+# In[10]:
 
-         
-@app.route("/get_client_predict_proba", methods=['GET'])
-def get_client_predict_proba():
+
+@app.route("/get_client_predict_proba/<cid>")
+def get_client_predict_proba2(cid):
     
-    cid=request.args.get('cid')
+    cid = int(cid) # transfo en integer à forcer
     
-    data_filtered = data.loc[data['SK_ID_CURR']==int(cid)]
-    data_filtered=data_filtered.drop(columns=['SK_ID_CURR'])
+    data_filtered = data.loc[data['SK_ID_CURR']==cid]
+    
     pred = model.predict_proba(data_filtered)
-    return str(pred[0])
+    return str(pred[0])  # mettre zéro le model retourne un dataframe
 
 
-# In[17]:
+# # LANCEMENT DE L API
+
+# In[24]:
 
 
 #LANCEMENT DE L API
 if __name__ == '__main__':
     print("L API a démarré !")
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False, port=8050)   # avant port 12993
     
     
 
 
-# In[4]:
+# In[21]:
 
 
-# In[5]:
+# data.columns. values. tolist()
 
 
-'''data1 = data[['EXT_SOURCE_1','EXT_SOURCE_2','EXT_SOURCE_3',
-                      'PAYMENT_RATE',
-                      'DAYS_BIRTH',
-                      'DAYS_EMPLOYED',
-                      'DAYS_EMPLOYED_PERC',
-                      'DAYS_REGISTRATION',
-                      'DAYS_ID_PUBLISH',
-                      'AMT_ANNUITY',
-                      'ANNUITY_INCOME_PERC',
-                      'REGION_POPULATION_RELATIVE',
-                      'DAYS_LAST_PHONE_CHANGE',
-                     'INCOME_CREDIT_PERC','INCOME_PER_PERSON']]
-data1'''
+# 
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# 
+
+# In[ ]:
+
+
+
+
+
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
 
 
 
