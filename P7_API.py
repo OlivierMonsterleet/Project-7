8b0b853f5a50d7df226826a009ee070d20340a4d
OlivierMonsterleet <146861945+OlivierMonsterleet@users.noreply.github.com>
2024-02-12 16:44:25 +0100

    Add files via upload

--- P7_API.py
+++ P7_API.py
@@ -6,13 +6,38 @@
 # In[ ]:
 
 
+# !pip install plost
 
+
+# In[20]:
+
+
+# pip install pytest
 
 
 # In[1]:
 
 
-from flask import Flask # ou fast API
+# pip install -U pytest
+import pytest
+
+
+# In[42]:
+
+
+pytest
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+from flask import Flask, request
 import requests
 import pickle
 import json
@@ -21,48 +46,57 @@ import pandas as pd
 import numpy as np
 
 
-# In[ ]:
-
-
-#!pip install plost
-
-
-# In[ ]:
-
-
-
-
-
-# In[4]:
-
-
-app = Flask(__name__)
-data = pickle.load(open('mydata.pkl','rb'))
-model = pickle.load(open('mymodel.pkl','rb'))
-app
-
-
-# In[2]:
-
-
-url = 'http://127.0.0.1:8050/'
-url
-
-
 # In[6]:
 
 
-#data = data.drop(columns=['index','TARGET'])
+import pkg_resources
+pkg_resources.get_distribution('flask').version
+
+
+# In[3]:
+
+
+app = Flask(__name__)
+
+
+data = pickle.load(open('data_opti_metier.pkl','rb'))   #----------------> 17 colonnes !!!!!!
+model = pickle.load(open('model_opti_metier.pkl','rb'))
+
+app
+
+
+# In[ ]:
+
+
+url = 'http://127.0.0.1:8050/'
+
+
+# In[47]:
+
+
+#data = data.drop(columns=['SK_ID_CURR','TARGET'])
+data
+
+
+# In[54]:
+
+
 data
 
 
 # In[ ]:
 
 
-data.dtypes
 
 
-# In[5]:
+
+# In[ ]:
+
+
+
+
+
+# In[69]:
 
 
 @app.route("/hello/")
@@ -70,22 +104,19 @@ def hello():
         return json.dumps("hello")
 
 
-# # get_all_data-->dictionnary
+# # get_all_data en dictionnary
 
-# In[6]:
+# In[70]:
 
 
 @app.route("/get_all_data/")
-def get_all_data5():
+def get_all_data():
     return (data.to_dict())
-    #return (data.csv())
-    #return (data.to_dict(orient='records'))
-    #return (data.to_json())
 
 
-# # get_all_data-->JSON
+# # get_all_data en JSON
 
-# In[7]:
+# In[71]:
 
 
 @app.route("/get_all_data_json/")
@@ -95,15 +126,14 @@ def get_all_data_json():
 
 # # get_client_data
 
-# In[8]:
+# In[72]:
 
 
 @app.route("/get_client_data/<cid>")
-def get_client_data17(cid):
+def get_client_data(cid):
     cid = int(cid) # transfo en integer à forcer
     data_filtered = data.loc[data['SK_ID_CURR']==cid]
 
-    #return (data_filtered.to_dict(orient='index'))
     return (data_filtered.to_json())
 
 
@@ -113,61 +143,59 @@ def get_client_data17(cid):
 
 
 
-# # get_prediction(client_id)
+# # get_prediction(client_id ex:370048)
 
-# In[23]:
+# In[9]:
 
 
 @app.route("/get_client_prediction/<cid>")
-def get_client_prediction5(cid):
+def get_client_prediction1(cid):
     cid = int(cid) # transfo en integer à forcer
-    
     data_filtered = data.loc[data['SK_ID_CURR']==cid]
-    data_filtered = data_filtered.drop(columns=['TARGET','SK_ID_CURR']) 
+    data_filtered = data_filtered.drop(columns=['TARGET','SK_ID_CURR'])  #---> on revient aux 15 champs du modèle
     pred = model.predict(data_filtered)
-    #prediction = classifier.predict_proba(filtered_data).tolist()
     if(pred>0.65): #[0][1]
         avis="Le crédit est refusé"
     else:
         avis="Le crédit est accepté"
     print(avis)
     print(pred)
-    #return {'prediction': avis, proba_rembour':round(prediction[0][0],2)}
-    return {str(pred[0]), avis}  # mettre zéro le model retourne un dataframe
+    return {str(pred[0]),avis}  # mettre zéro le model retourne un dataframe
     
 
 
-# In[10]:
+# # get_client_predict_proba
+
+# In[4]:
 
 
 @app.route("/get_client_predict_proba/<cid>")
-def get_client_predict_proba2(cid):
+def get_client_predict_proba1(cid):
     
     cid = int(cid) # transfo en integer à forcer
     
     data_filtered = data.loc[data['SK_ID_CURR']==cid]
-    
+    data_filtered = data_filtered.drop(columns=['TARGET','SK_ID_CURR'])  #---> on revient aux 15 champs du modèle
     pred = model.predict_proba(data_filtered)
     return str(pred[0])  # mettre zéro le model retourne un dataframe
 
 
 # # LANCEMENT DE L API
 
-# In[24]:
+# In[10]:
 
 
-#LANCEMENT DE L API
 if __name__ == '__main__':
     print("L API a démarré !")
-    app.run(debug=False, port=8050)   # avant port 12993
+    app.run(debug=False, port=8050)  
     
     
 
 
-# In[21]:
+# In[6]:
 
 
-# data.columns. values. tolist()
+data.columns. values. tolist()
 
 
 # 
@@ -184,34 +212,28 @@ if __name__ == '__main__':
 
 
 
-# In[ ]:
+# In[58]:
 
 
+data
+# 196588
 
 
-
-# In[ ]:
-
+# In[68]:
 
 
+cid = 370048
+#data = data.drop(columns=['SK_ID_CURR'])
+data_filtered = data.loc[data['SK_ID_CURR']==cid]
+data_filtered = data_filtered.drop(columns=['TARGET','SK_ID_CURR'])
+pred = model.predict_proba(data_filtered)
+pred
 
 
-# In[ ]:
+# In[65]:
 
 
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
+data_filtered
 
 
 # In[ ]:
@@ -227,6 +249,12 @@ if __name__ == '__main__':
 
 
 # In[ ]:
+
+
+
+
+
+# In[40]:
 
 
 
