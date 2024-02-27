cc149b71134f5731103b806615b32d1376471e4b
OlivierMonsterleet <146861945+OlivierMonsterleet@users.noreply.github.com>
2024-02-27 13:30:10 +0100

    Add files via upload

---
+++ test_API.py
@@ -0,0 +1,55 @@
+import pytest
+
+import pandas as pd
+import pytest
+import pickle
+from flask import request
+import requests
+import json
+
+import P7_API
+
+from P7_API import app
+
+# +
+#import sys 
+#sys.path
+
+# +
+#
+# -
+
+## Création d'un client
+client = app.test_client()
+
+
+# +
+# #client?
+# -
+
+def test_get_all_data_json():
+    response = client.get('/get_all_data_json')
+    data = json.loads(response.text)
+    df = pd.DataFrame.from_dict(data)
+    assert response.status_code == 200
+
+
+def test_get_client_prediction():
+    response = client.get('/get_client_prediction?cid=370048')
+    data = json.loads(response.text)
+    assert data == 0.0
+    
+def test_get_client_prediction2():
+    response = client.get('/get_client_prediction?cid=370048')
+    data = json.loads(response.text)
+    assert response.status_code == 200
+
+def test_get_client_predict_proba():
+    response = client.get('/get_client_predict_proba?cid=370048')
+    data = json.loads(response.text)
+    assert response.status_code == 200
+    
+#def test_get_client_shap():
+#    response = client.get('/get_client_shap?cid=370048')
+#    data = json.loads(response.text)
+#    assert response.status_code == 200
